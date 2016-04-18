f8311f670a944cbb1ad9ab535b10cf532e3b1726
Thomas Musser <tmusser@dataxu.com>
2016-04-18 19:33:09 -0400

    Getting V1 protoype for scatter/carto

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 var templates = utils.setupTemplates();  // Store templates for rendering tooltips and Edition details.
 
+// Will be used to the save the loaded JSON data
+var allData = [];
+loadData();
+
+function loadData() {
+    d3.json("data/processed_data/factors.json", function(error, jsonData){
+        if(!error){
+            allData = jsonData;
+
+            createVis();
+        }
+    });
+}
+
+function createVis() {
+
+    // Instantiate visualization objects here
+
+    cartogramChart = new CartogramChart("cartoGram", allData);
+}
